ca200c3d10fcd43892f1e01e897221f65c218e69
Daniel Rocha <danrochamaia@gmail.com>
2023-08-07 04:12:03 -0300

    first commit playwright G1

---
+++ playwrightG1.py
@@ -0,0 +1,49 @@
+from playwright.sync_api import sync_playwright
+
+subject = "Lula"
+pageNumber = 1
+maximumPages = 2
+
+def getPortalAndTime(text):
+        text = element.query_selector('.widget--info__meta--card').text_content().replace("\n", "").strip()
+        data = text.replace("    ","").split("•")
+        portal = data[0]
+        time = data[1]
+        return portal, time
+
+def getContent(className):
+    try:
+        return element.query_selector(f'.{className}').text_content()
+    except Exception:
+        return None
+
+with open('dados.txt', 'w') as file:
+    with sync_playwright() as p:
+        browser = p.chromium.launch()
+        page = browser.new_page()
+
+        while pageNumber <= maximumPages:
+            file.write(f"[PÁGINA {pageNumber}]\n\n")
+            page.goto(f"https://g1.globo.com/busca/?q={subject}&page={pageNumber}&ajax=1")
+            elements = page.query_selector('.results__list').query_selector_all('.widget')
+
+            if elements:
+                for element in elements:
+                    info = getContent("widget--info__meta")
+                    title = getContent("widget--info__title")
+
+                    if(info != None):
+                        portal, time = getPortalAndTime(info)
+                        file.write(f"PORTAL: {portal}\n")
+                        file.write(f"DATA: {time}\n")
+                    
+                    if(title != None):
+                        title = title.replace("\n","").replace("  ","")
+                        file.write(f"TÍTULO: {title}\n")
+
+                    if(info != None or title != None):
+                        file.write(f"LINK: {element.query_selector('a').get_attribute('href')}\n\n")
+                
+            pageNumber += 1
+
+        browser.close()
